d49db059be987e1078da06267f0aac79f2ea9d96
Nicholas De Souza <nd@example.com>
2017-08-27 22:34:31 -0400

    Added surepay locations map

--- src/app/headerbar/headerbar.component.ts
+++ src/app/headerbar/headerbar.component.ts
@@ -18,4 +18,8 @@ export class HeaderbarComponent implements OnInit {
     window.open("https://www.google.com/maps/d/viewer?mid=1XII2dGmmObI_xvcXV2ARWTvPuaY", "_blank")
   }
 
+  surepayMap(){
+    window.open("https://www.google.com/maps/d/u/0/viewer?mid=1FjSB2T6KufyHo8q29pph27_B8jM&hl=en", "_blank")
+  }
+
 }
