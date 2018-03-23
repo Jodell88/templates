1c13ad2529c47bd8d3d65bdb8f7d3d619ce94e86
Nicholas De Souza <nd@example.com>
2018-03-23 01:24:04 -0400

    Created technical voice template

--- src/app/technical-voice/technical-voice.component.ts
+++ src/app/technical-voice/technical-voice.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+export class TechVoice{
+  constructor(
+    public software?: string,
+    public xml?: string,
+    public preconfig?: number,
+    public pots?: string,
+    public mac?: string,
+    public dhcp?: number,
+    public tel1?: number,
+    public handset?: string,
+    public registered?: string,
+    public make?: string,
+    public receive?: string,
+    public dunned?: string,
+    public uni?: string,
+    public voip?: string,
+  ){}
+}
+
 @Component({
-  selector: 'app-technical-voice',
+  selector: 'technical-voice',
   templateUrl: './technical-voice.component.html',
   styleUrls: ['./technical-voice.component.css']
 })
 export class TechnicalVoiceComponent implements OnInit {
 
+  title = "Voice Technical"
+  model = new TechVoice
   constructor() { }
 
   ngOnInit() {
   }
 
+  reset(){
+    this.model = new TechVoice()
+  }
 }
