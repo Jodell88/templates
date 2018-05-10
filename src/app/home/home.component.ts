5686eb381bfd0f70f70b3d12a5f44c592ee6d60d
Nicholas De Souza <nd@example.com>
2018-05-10 12:01:40 -0400

    Added links to commonly used applications in work

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,31 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  crm(){
+    window.open("http://mcl-vm2-crm/MassyCommunicationsLtd/main.aspx#849628433", "_blank")
+  }
+
+  dlo(){
+    window.open("http://provisioning.massycommunications.com:8080/webclient/", "_blank")
+  }
+
+  fieldService(){
+    window.open("https://fieldservice.massycommunications.com/dispatchFS/", "_blank")
+  }
+
+  garfield(){
+    window.open("https://garfield.massycommunications.com/MCL_610/getPhoneNumber/1", "_blank")
+  }
+
+  tableau(){
+    window.open("http://tableau.massycommunications.com/", "_blank")
+  }
+
+  mail(){
+    window.open("http://tableau.massycommunications.com/", "_blank")
+  }
+
+  citrix(){
+    window.open("https://citrix-access.tstt.co.tt/", "_blank")
+  }
 }
